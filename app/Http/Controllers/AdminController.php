aa24bd6713a251ebd081a9f3edf25f39c5210e18
swk <kswcks1457@naver.com>
2021-05-23 14:09:57 +0900

    sprint4

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -17,8 +17,8 @@ class AdminController extends Controller
 
     public function admin(Request $request, $sort=null){
         $condition = array();
-        if($s = $request->filled('sdate')) { $condition[] = ['ldate', '>', $request->sdate]; session(['sdate' => $request->sdate]); };
-        if($l = $request->filled('ldate')) { $condition[]= ['sdate', '<', $request->ldate]; session(['ldate' => $request->ldate]); };
+        if($s = $request->filled('sdate')) { $condition[] = ['ldate', '>=', $request->sdate]; session(['sdate' => $request->sdate]); };
+        if($l = $request->filled('ldate')) { $condition[]= ['sdate', '<=', $request->ldate]; session(['ldate' => $request->ldate]); };
         if($s && $l) {
 
 
